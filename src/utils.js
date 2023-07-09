08298d26e141384fa73e9930bc04f40eb2c8a02d
gabyBarrientos12 <gabrielbarrientos729@gmail.com>
2023-07-09 20:30:19 -0300

    add bcrypt and functions hash

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,28 @@
 import {fileURLToPath} from 'url';
 import {dirname} from 'path';
+import bcrypt from 'bcrypt'
+
+/* 
+
+    - hashSync toma el password que pasemos y procedera a aplicar un proceso de hasheo a partir de un Salt
+    - getSaltSync generara un Salt de 10 caracteres. un Salt es un string random
+    - la funcion createHash devuelve un string con el password hasheado.
+
+*/
+export const createHash = password => bcrypt.hashSync(password , bcrypt.genSaltSync(10))
+
+/* 
+
+    - compareSync tomara primero el password sin hashear y lo comparara con el password hasheado en
+    la base de datos. y retorna true o false
+
+*/
+
+export const isValidPassword = (user , password) => bcrypt.compareSync( password, user.password)
+
+
+
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
